fcd0ba34a68273e1b639cbcb5e0061baa49bbe4a
Caio Nascimento <contato@caionascimento.com.br>
2022-08-03 11:06:37 -0300

    criação do layout de detalhes com sidebar

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,6 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: %i[ show edit update destroy ]
+  layout "clients/actions", only: [:show, :edit]
 
   # GET /clients or /clients.json
   def index
